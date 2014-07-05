2c80df3cd5b5b1717dbf0e09c0234ac65058ee91
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2014-07-05 22:56:55 +0900

    検索用モジュールを手直し

--- lib/amazon.rb
+++ lib/amazon.rb
@@ -6,7 +6,7 @@ require 'amazon/ecs'
 require "open-uri"
 require 'shellwords'
 
-module Amazon  
+module AmazonEcs
   SortParams = {
     "Books" => "daterank",
     "Music" => "-orig-rel-date",
@@ -17,20 +17,20 @@ module Amazon
     "VideoGames" => "-releasedate",
     "KindleStore" => "daterank"
   }
-  
+
   #EANからの商品情報取得
   def self.get(ean)
     page, item = search(ean, "blended")
     return item[0]||{}
   end
-  
+
   #キーワード商品検索
   def self.search(keyword, category, page = 1)
     category = category.capitalize
     category = "DVD" if category == "Dvd"
     category = "VideoGames" if category == "Videogames"
     category = 'KindleStore' if category == 'Kindlestore'
-    
+
         #キーワードをそれぞれダブルクォートで囲む
     begin
       words = []
@@ -41,37 +41,37 @@ module Amazon
     rescue ArgumentError
       words = keyword
     end
-    
-    src = Amazon::Ecs.item_search(words, 
+
+    src = Amazon::Ecs.item_search(words,
       :country => 'jp',
       :search_index => category,
       :response_group => 'ItemAttributes,Images',
       :sort => SortParams[category],
       :item_page => page
     )
-    
+
     total_result = src.total_results
-    
+
     ret = []
     src.items.each do |e|
       product = {}
       product[:ean] = e.get('ItemAttributes/EAN')
-      
+
       if product[:ean].present?
         product[:category] = category_normalize e.get('ItemAttributes/ProductGroup')
         product[:a_title] = e.get("ItemAttributes/Title")
         product[:a_url] = e.get("DetailPageURL")
-        
+
         manufacturer = e.get("ItemAttributes/Manufacturer")
         manufacturer = e.get("ItemAttributes/Publisher") if manufacturer.nil?
         product[:a_manufacturer] = manufacturer
-        
+
         release_date = e.get("ItemAttributes/PublicationDate")
         if release_date.nil?
           release_date = e.get("ItemAttributes/ReleaseDate")
         end
         fixed = true
-        
+
         release_date = release_date.to_s
         if release_date =~ /^[0-9][0-9][0-9][0-9]-[0-9][0-9]$/
           fixed = false
@@ -80,33 +80,33 @@ module Amazon
           fixed = false
           release_date += '-01-01'
         end
-        
+
         release_date = Time.parse(release_date)
         product[:a_release_date] = release_date
         product[:a_release_date_fixed] = fixed
-        
+
         product[:a_image_medium] = e.get("MediumImage/URL")
         product[:a_image_small] = e.get("SmallImage/URL")
-        
+
         product[:a_authors] = e.get_array("ItemAttributes/Author")
         product[:a_authors].push e.get_array("ItemAttributes/Artist")
         product[:a_authors].push e.get_array("ItemAttributes/Director")
         product[:a_authors].flatten!
-        
+
         ret.push(product)
       end
     end
-    
+
     return total_result, ret
   end
-  
+
   def self.sign(query)
     sorted = query.sort
     message = ["GET", Host, Path, sorted.join("&")].join("\n")
-    sign = hmac_sha256(Ishibashi::Application.config.amazon[:secret_key], message)
+    sign = hmac_sha256(Amazon::Ecs.options[:AWS_secret_key], message)
     return Base64.encode64(sign).chomp
   end
-  
+
   IPAD = "\x36"
   OPAD = "\x5c"
   def self.hmac_sha256(key, message)
@@ -116,15 +116,15 @@ module Amazon
       ikey[i] = key[i] ^ ikey[i]
       okey[i] = key[i] ^ okey[i]
     end
-  
+
     value = Digest::SHA256.digest(ikey + message)
     value = Digest::SHA256.digest(okey + value)
   end
-  
+
   def self.category_normalize(src)
     src = src.gsub(' ', '').downcase
     src = 'books' if src == 'book'
-    
+
     return src
   end
 end
--- lib/rakuten_books.rb
+++ lib/rakuten_books.rb
@@ -12,13 +12,13 @@ module RakutenBooks
     "software" => "004",
     "blended" => "000"
   }
-  
+
   #EANからの商品情報取得
   def self.get(ean)
     page, item = search(ean, "blended")
     return item[0]||{}
   end
-  
+
   #キーワード商品検索
   def self.search(keyword, category, page = 1)
     params = []
@@ -31,20 +31,20 @@ module RakutenBooks
       "field" => "0",
       "keyword" => keyword,
       "booksGenreId" => Genre[category]}).each do |key, value|
-      
+
       params.push "#{key}=#{CGI::escape value}"
     end
     request_url = "http://api.rakuten.co.jp/rws/2.0/json?#{params.join "&"}"
-    
+
     if defined?(OpenURI)
       #puts request_url
       src = open(request_url).read
     else
       src = AppEngine::URLFetch.fetch(request_url).body
     end
-    
+
     data = JSON.parse src
-    
+
     ret = []
     total_result = 0
     #データ存在チェック
@@ -54,39 +54,43 @@ module RakutenBooks
       data["Body"]["BooksTotalSearch"]["Items"]["Item"].each do |item|
         ean = item["jan"]
         ean = item["isbn"] if ean == ""
-        
+
         manufacturer = item["publisherName"]
         manufacturer = item["label"] if manufacturer == ""
-        
+
         author = item["author"]
         author = item["artistName"] if author == ""
-        
+
         authors = [author] unless author.empty?
         authors = nil if author.empty?
-        
+
         if item["mediumImageUrl"] == "http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/noimage_01.gif?_ex=120x120"
           item.delete("mediumImageUrl")
         end
-        
+
         if item["smallImageUrl"] == "http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/noimage_01.gif?_ex=64x64"
           item.delete("smallImageUrl")
         end
-        
-        target = {
-          :ean => ean,
-          :r_title => item["title"],
-          :r_url => item["affiliateUrl"],
-          :r_manufacturer => manufacturer,
-          :r_release_date => Time.parse(item["salesDate"]),
-          :r_image_medium => item["mediumImageUrl"],
-          :r_image_small => item["smallImageUrl"],
-          :r_authors => authors
-        }
-        
-        ret.push target
+
+        begin
+          release_date = Time.parse(item["salesDate"])
+          ret.push(
+            :ean => ean,
+            :r_title => item["title"],
+            :r_url => item["affiliateUrl"],
+            :r_manufacturer => manufacturer,
+            :r_release_date => release_date,
+            :r_image_medium => item["mediumImageUrl"],
+            :r_image_small => item["smallImageUrl"],
+            :r_authors => authors
+          )
+        rescue ArgumentError
+          # do nothing
+          # 発売日が不正
+        end
       end
     end
-    
+
     return total_result, ret
   end
 end
