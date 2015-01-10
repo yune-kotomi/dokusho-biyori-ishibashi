c5367d062739a39323b2c04fd54947f4d934174c
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-01-10 14:45:55 +0900

    ランダムURL有効時にフィードへのリンクが正しくない問題を修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,34 @@
 module UsersHelper
+  def rss_feed_url(user)
+    if user.private && user.random_url
+      id = user.random_key
+    else
+      id = user.id
+    end
+
+    url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => id,
+      :format => :rdf,
+      :only_path => false
+    )
+  end
+
+  def ics_feed_url(user, protocol = nil)
+    if user.private && user.random_url
+      id = user.random_key
+    else
+      id = user.id
+    end
+
+    url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => id,
+      :format => :ics,
+      :only_path => false,
+      :protocol => protocol
+    )
+  end
 end
--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -1,4 +1,90 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
+  include UsersHelper
+
+  setup do
+    @public = users(:user1)
+    @private1 = users(:user2)
+    @private2 = users(:user3)
+  end
+
+  test "公開ユーザのRSSフィードURLはユーザIDを含む" do
+    actual = rss_feed_url(@public)
+    expect = url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => @public.id,
+      :format => :rdf,
+      :only_path => false
+    )
+
+    assert_equal expect, actual
+  end
+
+  test "ランダムURLが無効な非公開ユーザのRSSフィードURLはユーザIDを含む" do
+    actual = rss_feed_url(@private1)
+    expect = url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => @private1.id,
+      :format => :rdf,
+      :only_path => false
+    )
+
+    assert_equal expect, actual
+  end
+
+  test "ランダムURLが有効な非公開ユーザのRSSフィードURLはランダムキーを含む" do
+    actual = rss_feed_url(@private2)
+    expect = url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => @private2.random_key,
+      :format => :rdf,
+      :only_path => false
+    )
+
+    assert_equal expect, actual
+  end
+
+
+  test "公開ユーザのiCalendarフィードURLはユーザIDを含む" do
+    actual = ics_feed_url(@public)
+    expect = url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => @public.id,
+      :format => :ics,
+      :only_path => false
+    )
+
+    assert_equal expect, actual
+  end
+
+  test "ランダムURLが無効な非公開ユーザのiCalendarフィードURLはユーザIDを含む" do
+    actual = ics_feed_url(@private1)
+    expect = url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => @private1.id,
+      :format => :ics,
+      :only_path => false
+    )
+
+    assert_equal expect, actual
+  end
+
+  test "ランダムURLが有効な非公開ユーザのiCalendarフィードURLはランダムキーを含む" do
+    actual = ics_feed_url(@private2)
+    expect = url_for(
+      :controller => :users,
+      :action => :feeds,
+      :id => @private2.random_key,
+      :format => :ics,
+      :only_path => false
+    )
+
+    assert_equal expect, actual
+  end
 end
