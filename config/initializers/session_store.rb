2b6c2c9ac71d45aa3038e74f78cb77651ed9cf98
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-01-12 11:26:38 +0900

    セッション有効期限を1ヶ月に延長

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Ishibashi::Application.config.session_store :cookie_store, key: '_ishibashi_session'
+Ishibashi::Application.config.session_store :cookie_store,
+  :key => '_ishibashi_session',
+  :expire_after => 1.month
