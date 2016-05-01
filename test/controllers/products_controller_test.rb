e7b84ab23fd3d813aee4a8b2b7084eece14c5e7a
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2016-05-01 23:16:03 +0900

    指定商品一覧ページ実装 close #10

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,12 +3,23 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:keyword_search1)
+    @products = [
+      products(:keyword_search2),
+      products(:product1)
+    ]
   end
 
   test "should show product" do
     get :show, id: @product.ean
     assert_response :success
     assert_equal @product, assigns(:product)
+  end
+
+  test '複数商品表示' do
+    get :show, :id => @product.ean, :ean => @products.map(&:ean)
+    assert_response :success
+    assert_equal @product, assigns(:product)
+    assert_equal @products, assigns(:products)
   end
 
   test "Amazonへのリダイレクト" do
