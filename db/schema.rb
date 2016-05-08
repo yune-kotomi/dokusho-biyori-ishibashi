908105abfd2e173ff21fb44dc878c4095b5a1894
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2016-05-08 22:45:21 +0900

    プロフィール画像表示に対応

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508070550) do
+ActiveRecord::Schema.define(version: 20160508134126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20160508070550) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.jsonb    "tags"
+    t.string   "profile_image"
   end
 
 end
